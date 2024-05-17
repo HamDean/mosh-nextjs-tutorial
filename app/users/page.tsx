8dcd372197f4a0d1becb9dd07257b236bb8f49e8
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-17 12:35:28 +0000

    navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link"
+
 const Users = () => {
   return (
+    <>
     <div>Users</div>
+    <Link href={'/users/userdetail'}>user detail</Link>
+    </>
   )
 }
 export default Users
