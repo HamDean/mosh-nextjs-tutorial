7b3d935866949f7ae5470977d3b4be7a595acb3d
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-18 17:54:11 +0000

    build the users table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,21 +3,32 @@ import Link from "next/link";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const Users = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: 'no-cache'
-  });
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
 
   return (
     <>
       <div>Users</div>
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li> )}
-      </ul>
-      <Link href={"/users/userdetail"}>user detail</Link>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="cursor-pointer">Name</th>
+            <th className="cursor-pointer">email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
