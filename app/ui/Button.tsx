316f8394f790dea40402db3fa27e35c11f17e9c6
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-18 17:49:29 +0000

    intalling daisy ui

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
 const Button = () => {
-  return <div onClick={() => console.log("clicked")}>Add to Cart</div>;
+  return <div className="btn btn-primary" onClick={() => console.log("clicked")}>Add to Cart</div>;
 };
 export default Button;
