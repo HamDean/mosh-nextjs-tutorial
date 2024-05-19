ffccfe9f95474e58d6fd0d71a09efbafa7e59ebd
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-19 18:06:40 +0000

    Extract the table element into a separate component

---
+++ app/users/Table.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { sort } from "fast-sort";
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+
+const Table = async ({ sortOrder}: { sortOrder: string}) => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users: User[] = await res.json();
+  
+    const sortedUsers = sort(users).asc(
+      user => sortOrder === 'email' ? user.email : user.name
+    )
+  return (
+    <table className="table">
+        <thead>
+          <tr>
+            <th className="cursor-pointer">
+              <Link href={"/users?sortOrder=name"}>Name</Link>
+            </th>
+            <th className="cursor-pointer">
+              <Link href={"/users?sortOrder=email"}>email</Link>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedUsers.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  )
+}
+export default Table
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,47 +1,14 @@
-import Link from "next/link";
-import { sort } from "fast-sort";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import Table from "./table";
 
 const Users = async ({
   searchParams: { sortOrder },
 }: {
   searchParams: { sortOrder: string };
 }) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
-
-  const sortedUsers = sort(users).asc(
-    user => sortOrder === 'email' ? user.email : user.name
-  )
-
   return (
     <>
       <h1>Users</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="cursor-pointer">
-              <Link href={"/users?sortOrder=name"}>Name</Link>
-            </th>
-            <th className="cursor-pointer">
-              <Link href={"/users?sortOrder=email"}>email</Link>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedUsers.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Table sortOrder={sortOrder} />
     </>
   );
 };
