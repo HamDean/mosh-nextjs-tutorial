2f377481fd96c67b3563960a51c3040a2dd05589
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-20 20:52:19 +0000

    creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,9 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const users = [
+  { id: 1, name: "a" },
+  { id: 2, name: "aa" },
+  { id: 3, name: "aaa" },
+];
+
 export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "a" },
-    { id: 2, name: "aa" },
-    { id: 3, name: "aaa" },
-  ]);
+  return NextResponse.json(users);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const user = {
+    id: users.length + 1,
+    name: body.name,
+  };
+  users.push(user);
+
+  return NextResponse.json(user);
 }
