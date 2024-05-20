0ec5292592d7b50546ebca8299db65bd6d9017bf
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-20 16:37:52 +0000

    Building API - GET route handler

---
+++ app/api/users/route.tsx
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest){
+    return NextResponse.json([
+        { id: 1, name: 'a'},
+        { id: 2, name: 'aa'},
+        { id: 3, name: 'aaa'},
+    ])
+}
