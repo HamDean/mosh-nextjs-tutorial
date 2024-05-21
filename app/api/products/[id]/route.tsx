b0300f1970f38128fbdb046c30a9cd6737d268be
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-21 21:29:07 +0000

    getting a product by id

---
+++ app/api/products/[id]/route.tsx
@@ -0,0 +1,11 @@
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(
+  request: NextRequest,
+  { params: { id } }: { params: { id: number } }
+) {
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: id, name: "Apple", price: 5.0 });
+}
