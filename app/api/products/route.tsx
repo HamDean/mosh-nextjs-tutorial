b4d8bbfe2662283801cae3a69e58fbec53497c46
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-21 21:33:34 +0000

    creating a product - POST method

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,9 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const products = [
+  { id: 1, name: "Milk", price: 2.4 },
+  { id: 2, name: "Sugar", price: 1.4 },
+  { id: 3, name: "Bread", price: 2.7 },
+];
+
 export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.4 },
-    { id: 2, name: "Sugar", price: 1.4 },
-    { id: 3, name: "Bread", price: 2.7 },
-  ]);
+  return NextResponse.json(products);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const newProduct = {
+    id: products.length + 1,
+    name: body.name,
+    price: body.price,
+  };
+
+  products.push(newProduct);
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
