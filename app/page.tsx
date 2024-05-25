fbf8f28e369726fba89459387de9c3b2516b53b8
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-25 18:03:03 +0000

    set up shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <main>
       <h1>Hello {session && <span>{session.user?.name}</span>}</h1>
+      <Button variant="outline">Shadcn</Button>
       <Link href={"/users"}>users</Link>
     </main>
   );
