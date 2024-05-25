48663c6f6dace1ba5aea69c76000dcb27037a5b2
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-25 18:22:40 +0000

    build form structure

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,62 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const schema = z.object({
+  username: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(5),
+});
+
+type formData = z.infer<typeof schema>;
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const form = useForm<formData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const onSubmit = (values: formData) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 export default SignupPage;
